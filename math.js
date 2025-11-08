bcd8c8d04a9ce3c6ee50c2665ecb5fa6f0c12741
najmabashiir <najaxbashiirali>
2025-11-08 17:37:10 +0300

    to use shortcut in module.exports to exports
    Signed-off-by: najmabashiir <najaxbashiirali>

--- math.js
+++ math.js
@@ -1,10 +1,10 @@
-function add(a ,b ){
+exports.add=function add(a ,b ){
     return a + b;
 }
-function subtract(a,b){
+exports.subtract=function subtract(a,b){
     return a - b;
 }
 // module.exports.add=add;
 // module.exports.subtract=subtract
 
-module.exports={add, subtract}
+// module.exports={add, subtract}
